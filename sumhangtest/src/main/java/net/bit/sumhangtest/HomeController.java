09aa7a4040ed548846982300fcd7e01b33bf92c6
bit <bit@bitacademy21>
2014-07-22 09:43:36 +0900

    test

---
+++ sumhangtest/src/main/java/net/bit/sumhangtest/HomeController.java
@@ -0,0 +1,38 @@
+package net.bit.sumhangtest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	@Autowired
+    private SqlSession sqlSession;
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "json/addUser.json", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String addNewElement(@RequestBody String userItem){
+		System.out.println("addUser method invoked...");
+		return "home";
+	}
+	
+}
